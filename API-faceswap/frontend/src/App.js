407de5f44aba7fb40512f7466ea8b5d6c72b1339
NgTuanLoc <18521011@gm.uit.edu.vn>
2021-11-10 14:44:00 +0700

    KHOA API

---
+++ API-faceswap/frontend/src/App.js
@@ -0,0 +1,140 @@
+import { useState } from "react";
+
+import "./App.css";
+import axios from "axios";
+
+const App = () => {
+  const [previewImageUrl, setPreviewImageUrl] = useState(false);
+  const [imageHeight, setImageHeight] = useState(500);
+  const [imageFile, setImageFile] = useState(null);
+  const [imagePrediction, setImagePrediction] = useState("");
+
+  const generatePreviewImageUrl = (file, callback) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onloadend = (e) => callback(reader.result);
+  };
+
+  const handleChange = (e) => {
+    const file = e.target.files[0];
+    if (!file) {
+      return;
+    }
+
+    setImageFile(file);
+
+    generatePreviewImageUrl(file, (previewImageUrl) => {
+      setImagePrediction("");
+      setPreviewImageUrl(previewImageUrl);
+    });
+  };
+
+  // Function for sending image to the backend
+  const uploadHandler = (e, image_name) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", imageFile, `${image_name}.png`);
+
+    console.log(formData);
+    let t0 = performance.now();
+    axios
+      .post("http://localhost:5000/upload", formData)
+      .then(function (response, data) {
+        data = response.data;
+        // setPreviewImageUrl(data);
+        let t1 = performance.now();
+        console.log(
+          "The time it took to predict the image " +
+            (t1 - t0) +
+            " milliseconds."
+        );
+      });
+  };
+
+  const objectDetectionHandler = () => {
+    let t0 = performance.now();
+    axios.get("http://localhost:5000/predict").then(function (response, data) {
+      data = response.data;
+      setPreviewImageUrl(data);
+      let t1 = performance.now();
+      console.log(
+        "The time it took to predict the image " + (t1 - t0) + " milliseconds."
+      );
+    });
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="App-upload">
+          <p>Upload an image for classification</p>
+
+          {/* Button for choosing an image */}
+          <div>
+            <input type="file" name="file" onChange={handleChange} />
+          </div>
+
+          {/* Button for sending image to backend */}
+          <div>
+            <button
+              type="submit"
+              onClick={(event) => uploadHandler(event, "img_1")}
+            >
+              Submit
+            </button>
+          </div>
+
+          {/* Field for previewing the chosen image */}
+          <div>
+            {previewImageUrl && (
+              <img height={imageHeight} alt="" src={previewImageUrl} />
+            )}
+          </div>
+
+          {/* Text for model prediction */}
+          <div>
+            {imagePrediction && <p>The model predicted: {imagePrediction}</p>}
+          </div>
+        </div>
+        <div className="App-upload">
+          <p>Upload an image for classification</p>
+
+          {/* Button for choosing an image */}
+          <div>
+            <input type="file" name="file" onChange={handleChange} />
+          </div>
+
+          {/* Button for sending image to backend */}
+          <div>
+            <button
+              type="submit"
+              onClick={(event) => uploadHandler(event, "img_2")}
+            >
+              Submit
+            </button>
+          </div>
+
+          {/* Field for previewing the chosen image */}
+          <div>
+            {previewImageUrl && (
+              <img height={imageHeight} alt="" src={previewImageUrl} />
+            )}
+          </div>
+
+          {/* Text for model prediction */}
+          <div>
+            {imagePrediction && <p>The model predicted: {imagePrediction}</p>}
+          </div>
+        </div>
+        <div>
+          <button type="submit" onClick={() => objectDetectionHandler()}>
+            Submit
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default App;
